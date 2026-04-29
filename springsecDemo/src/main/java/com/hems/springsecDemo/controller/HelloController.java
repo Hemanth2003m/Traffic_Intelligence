caf6fd8e1e155a9a3aff027b353e0f6bfd05c985
Chitikela Hemanth <22p31a05f1@acet.ac.in>
2026-04-29 18:55:24 +0530

    Update HelloController.java

--- springsecDemo/src/main/java/com/hems/springsecDemo/controller/HelloController.java
+++ springsecDemo/src/main/java/com/hems/springsecDemo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("hello")
     public String greet(){
-        return "Hello Ladies";
+        return "Byeeee Ladies";
     }
 }
